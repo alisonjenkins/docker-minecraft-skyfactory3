c383ceefabfc682ba209ea3958474e2890e7309c
Alan Jenkins <alan.jenkins@claranet.uk>
2017-07-23 06:56:41 +0100

    Disable override directories for testing.

--- start_mc.sh
+++ start_mc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 VAR=$(cat <<'ENDCONFIG'
 generator-settings=
 op-permission-level=4
@@ -35,9 +38,22 @@ motd=Welcome to the Redwood Skyfactory3 Server
 ENDCONFIG
 )
 
-cp -R /srv/minecraft/config.override/* /srv/minecraft/config/
-cp -R /srv/minecraft/mods.override/* /srv/minecraft/mods/
+function run_command()
+{
+    echo "$1"
+    eval "$1"
+}
 
-echo "$VAR" > /srv/minecraft/server.properties
+run_command "addgroup -g $MCGID minecraft"
+run_command "adduser -h /srv/minecraft -S -u $MCUID -G minecraft minecraft"
+run_command "chown -R minecraft:minecraft /srv/minecraft"
 
-cd /srv/minecraft/ && java -server -d64 -XX:NewRatio=1 -XX:UseSSE=4 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:+UseCMSCompactAtFullCollection -XX:+CMSScavengeBeforeRemark -XX:+UseParNewGC -XX:SurvivorRatio=4 -XX:NewSize=128m -XX:MaxNewSize=128m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseLargePages -XX:LargePageSizeInBytes=2m -XX:+UseStringCache -XX:CompileThreshold=500 -XX:+UseFastAccessorMethods -XX:+UseBiasedLocking -XX:+OptimizeStringConcat -Dsun.net.client.defaultConnectTimeout=1000 -Xmx${MCMEM}M -Xms${MCMEM}M -jar FTBserver-1.10.2-12.18.3.2215-universal.jar nogui
+# run_command "cp -R /srv/minecraft/config.override/* /srv/minecraft/config/"
+# run_command "cp -R /srv/minecraft/mods.override/* /srv/minecraft/mods/"
+
+run_command "echo "${VAR}" > /srv/minecraft/server.properties"
+
+echo "Starting server"
+
+cd /srv/minecraft/
+run_command "su -c \"java -server -d64 -XX:NewRatio=1 -XX:UseSSE=4 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:+UseCMSCompactAtFullCollection -XX:+CMSScavengeBeforeRemark -XX:+UseParNewGC -XX:SurvivorRatio=4 -XX:NewSize=128m -XX:MaxNewSize=128m -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseLargePages -XX:LargePageSizeInBytes=2m -XX:+UseStringCache -XX:CompileThreshold=500 -XX:+UseFastAccessorMethods -XX:+UseBiasedLocking -XX:+OptimizeStringConcat -Dsun.net.client.defaultConnectTimeout=1000 -Xmx${MCMEM}M -Xms${MCMEM}M -jar FTBserver-1.10.2-12.18.3.2215-universal.jar nogui\" minecraft"
